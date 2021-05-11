47d46fc936723b96c47e85187eae7bac543839b5
sugarfixx <ingar.torsrud@me.com>
2021-05-12 00:16:00 +0200

    Add controller and methods

---
+++ app/Http/Controllers/RedirectController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 12/05/2021
+ * Time: 00:03
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class RedirectController extends Controller
+{
+    public function showForm()
+    {
+        return view('form');
+    }
+
+    public function processForm(Request $request)
+    {
+        $token = new \stdClass();
+        $token->session = $request->post('session');
+        $token->auth = $request->post('auth');
+        return $this->returnTarget($token);
+
+    }
+
+    public function returnTarget($token)
+    {
+
+        setcookie('Mediabank_', $token->session, time() + (3600 * 6), "/", '.mediabank.me');
+        setcookie('auth', $token->auth, time() + (3600 * 6), "/", '.mediabank.me');
+        return redirect()->to($this->getTargetFromAuth($token->auth));
+    }
+
+    private function getTargetFromAuth($auth)
+    {
+        return 'https://www.mediabank.me/apps/dashboard/';
+    }
+}
